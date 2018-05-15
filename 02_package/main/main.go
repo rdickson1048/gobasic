0a6e712335914f1d92899612228756c255ead791
rdickson1048 <rddicksoniii@gmail.com>
2018-05-15 18:29:40 +0200

    second commit

--- 02_package/main/main.go
+++ 02_package/main/main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/rdickson1048/gobasic/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG , olleH"))
+	fmt.Println(stringutil.MyName)
+}
